7a9495621c97c6eb7cac9dd49102d7d9dcbdce53
FinFET <76776190+FinFetChannel@users.noreply.github.com>
2022-03-04 23:41:08 -0300

    Update objLoader.py

--- objLoader.py
+++ objLoader.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# read wavefront models with or without textures, supports triangles and quads (turned into triangles)
 def read_obj(fileName):
     '''
     Read wavefront models with or without textures, supports triangles and quads (turned into triangles)
@@ -21,29 +20,23 @@ def read_obj(fileName):
             elif splitted[0] == "vt": # texture coordinates
                 texture_uv.append(splitted[1:3])
 
-
             elif splitted[0] == "f": # Faces
 
                 if len(splitted[1].split("/")) == 1: # no textures
-
                     triangles.append([splitted[1], splitted[2], splitted[3]])
 
                     if len(splitted) > 4: # quads, make additional triangle
                         triangles.append([splitted[1], splitted[3], splitted[4]])
 
                 else: # with textures
-
                     p1 = splitted[1].split("/")
                     p2 = splitted[2].split("/")
                     p3 = splitted[3].split("/")
-                    
                     triangles.append([p1[0], p2[0], p3[0]])
                     texture_map.append([p1[1], p2[1], p3[1]])
                     
                     if len(splitted) > 4: # quads, make additional triangle
-                        
                         p4 = splitted[4].split("/")
-                        
                         triangles.append([p1[0], p3[0], p4[0]])
                         texture_map.append([p1[1], p3[1], p4[1]])
                 
@@ -55,6 +48,7 @@ def read_obj(fileName):
         texture_uv = np.asarray(texture_uv).astype(float)
         texture_uv[:,1] = 1 - texture_uv[:,1] # apparently obj textures are upside down
         texture_map = np.asarray(texture_map).astype(int) - 1 # adjust indexes to start with 0
+        
     else:
         texture_uv, texture_map = np.asarray(texture_uv), np.asarray(texture_map)
         textured = False 
